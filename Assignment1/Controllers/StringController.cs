a780cf67140577c09ef164bb164727ab004ef2b8
Rohit Sharma <pc@DESKTOP-H94GUCO>
2022-01-27 15:23:41 -0500

    just removed some functions from comment.

--- Assignment1/Controllers/StringController.cs
+++ Assignment1/Controllers/StringController.cs
@@ -16,14 +16,11 @@ namespace Assignment1.Controllers
         // POST localhost/Greeting “Hello World!”
 
 
+        public string post()
+        {
+          return "Hello World";
 
-
-        // GET api/values/5
-       // public string post()
-        //{
-         //   return "Hello World";
-
-        //}
+        }
 
 
 
@@ -31,11 +28,8 @@ namespace Assignment1.Controllers
         // Request Response
         //GET localhost/Greeting/3 Greetings to 3 people!
         // GET localhost/Greeting/6 Greetings to 6 
-        //GET localhost/Greeting/0 Greetings to 0 people!
+        //GET localhost/Greeting/0 Greetings to 0 people
 
-
-
-        // GET api/values/5
         public string Get(int id)
         {
             String greeting = "Greeting to {id} people!";
